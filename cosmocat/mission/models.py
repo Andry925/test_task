3423e8176aa5f2273a803574d53e63e0c5c97387
Andry925 <andry925@ukr.net>
2024-11-20 18:39:21 +0200

    feat: implement functionality to mark mission completed when all tasks completed

--- cosmocat/mission/models.py
+++ cosmocat/mission/models.py
@@ -5,3 +5,8 @@ from spycat.models import Cat
 class Mission(models.Model):
     cat = models.ForeignKey(Cat, related_name='missions', on_delete=models.CASCADE, null=True, blank=True)
     completed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.targets.exists() and all(target.completed for target in self.targets.all()):
+            self.completed = True
+        super().save(*args, **kwargs)
