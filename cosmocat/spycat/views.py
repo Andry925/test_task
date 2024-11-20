9cb0f862cdd049cc4a758e6eb6e65309bfde34af
Andry925 <andry925@ukr.net>
2024-11-20 15:21:43 +0200

    feat: implement initial cats endpoints

--- cosmocat/spycat/views.py
+++ cosmocat/spycat/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Cat
+from .serializers import SpyCatSerializer, UpdateCatSerializer
+
+
+class SpycatListCreateView(generics.ListCreateAPIView):
+    serializer_class = SpyCatSerializer
+    queryset = Cat.objects.all()
+
+
+class SpycatRetrieveDestroyView(generics.RetrieveDestroyAPIView):
+    serializer_class = SpyCatSerializer
+    queryset = Cat.objects.all()
+
+
+class SpycatUpdateView(generics.UpdateAPIView):
+    serializer_class = UpdateCatSerializer
+    queryset = Cat.objects.all()
